7d9d5a93a0301da941e1731304271b01639280be
Siddhesh <siddheshghag150@gmail.com>
2025-05-21 00:30:40 +0530

    connected to neon db

--- server_side/config/db.js
+++ server_side/config/db.js
@@ -6,12 +6,12 @@ dotenv.config();
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
 });
 
 db.connect()
   .then(() => console.log("✅ PostgreSQL database connected"))
-  .catch((err) => console.error("❌ PostgreSQL connection error", err));
+  .catch((err) => console.error("❌ PostgreSQL connection error", err.message));
 
 export default db;
